6942796bb802dd2616831b91a5abbec7db9bb4b0
Geypen <christoph.geypen@groupepvcp.com>
2017-01-17 16:34:12 +0100

    working with getting acccount name

--- Budget tracker.js
+++ Budget tracker.js
@@ -1,34 +1,44 @@
 function main() {
-
-   var ss = SpreadsheetApp.openByUrl(
-       'https://docs.google.com/spreadsheets/d/1wxB3LdCzI3xdxhZxDNzdNRHOUzfadoeTWiFbjhVUVQs/edit#gid=0');
+   // config
+   var account = AdWordsApp.currentAccount().getName();
+   var spreadsheeturl = 'https://docs.google.com/spreadsheets/d/1wxB3LdCzI3xdxhZxDNzdNRHOUzfadoeTWiFbjhVUVQs/edit#gid=0'
+   
+   // code
+   var ss = SpreadsheetApp.openByUrl(spreadsheeturl);
 
    var sheet = ss.getSheets()[0];
    Logger.log(ss.getName());
    Logger.log(sheet);
 
+
   function getBudgetDetails() {
     var campaignIterator = AdWordsApp.campaigns()
-        //.withCondition('Name CONTAINS "_B_"')
+        .withCondition('Status = ENABLED')
         .get();
     while (campaignIterator.hasNext()) {
       var campaign = campaignIterator.next();
       var budget = campaign.getBudget();
       var budgetCampaignIterator = budget.campaigns().get();
-
-      Logger.log('Associated campaigns : ' +
-                 campaign.getName());
-      Logger.log('Budget amount : ' + budget.getAmount());
-      Logger.log('Delivery method : ' + budget.getDeliveryMethod());
-      Logger.log('Explicitly shared : ' + budget.isExplicitlyShared());
-      Logger.log('Associated campaigns : ' +
-          budgetCampaignIterator.totalNumEntities());
       var stats = campaign.getStatsFor('YESTERDAY');
-      Logger.log(stats.getCost() + ' cost');
+
+      var date = new Date();
+      date.setDate(date.getDate() - 1);
+
+      Logger.log('Associated campaigns : ' + campaign.getName());
       Logger.log('===NEXT===');
 
       // append a row to the defined spreadsheet
-      sheet.appendRow([campaign.getName(), budget.getAmount(), stats.getCost(), budget.getDeliveryMethod(), budget.isExplicitlyShared(), budgetCampaignIterator.totalNumEntities()]);
+      sheet.appendRow([
+        account,
+        date, 
+        campaign.getName(), 
+        budget.getAmount(), 
+        stats.getCost(), 
+        stats.getCost()/budget.getAmount(),
+        budget.getDeliveryMethod(), 
+        budget.isExplicitlyShared(), 
+        budgetCampaignIterator.totalNumEntities()
+        ]);
 
     }
   }
