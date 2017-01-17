981a7c649398449e777e29c36bea84d35bd95cbe
Geypen <christoph.geypen@groupepvcp.com>
2017-01-17 15:35:39 +0100

    get copy data to google sheets fixed

--- Budget tracker.js
+++ Budget tracker.js
@@ -1,4 +1,12 @@
 function main() {
+
+   var ss = SpreadsheetApp.openByUrl(
+       'https://docs.google.com/spreadsheets/d/1wxB3LdCzI3xdxhZxDNzdNRHOUzfadoeTWiFbjhVUVQs/edit#gid=0');
+
+   var sheet = ss.getSheets()[0];
+   Logger.log(ss.getName());
+   Logger.log(sheet);
+
   function getBudgetDetails() {
     var campaignIterator = AdWordsApp.campaigns()
         //.withCondition('Name CONTAINS "_B_"')
@@ -8,6 +16,8 @@ function main() {
       var budget = campaign.getBudget();
       var budgetCampaignIterator = budget.campaigns().get();
 
+      Logger.log('Associated campaigns : ' +
+                 campaign.getName());
       Logger.log('Budget amount : ' + budget.getAmount());
       Logger.log('Delivery method : ' + budget.getDeliveryMethod());
       Logger.log('Explicitly shared : ' + budget.isExplicitlyShared());
@@ -16,13 +26,9 @@ function main() {
       Logger.log('Details');
       Logger.log('=======');
 
-      // Get all the campaigns associated with this budget. There could be
-      // more than one campaign if this is a shared budget.
+      // append a row to the defined spreadsheet
+      sheet.appendRow([campaign.getName(), budget.getAmount(), budget.getDeliveryMethod(), budget.isExplicitlyShared(), budgetCampaignIterator.totalNumEntities()]);
 
-      while (budgetCampaignIterator.hasNext()) {
-        var associatedCampaign = budgetCampaignIterator.next();
-        Logger.log(associatedCampaign.getName());
-      }
     }
   }
 
