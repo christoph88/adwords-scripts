b2114669020d2f71f64ba695320ea03ab885a794
Christoph Geypen <geypenchristoph@gmail.com>
2018-06-01 08:03:42 +0000

    renamed all to .js and added new mass campaign and adgroup rename scripts

---
+++ BE_Mass_Adgroup_Rename.js
@@ -0,0 +1,102 @@
+// log to this spreadsheet
+var SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1KzxwSwHKTrHLC4uTpvbubcsOPOHsut95EpjyGYU99mA/edit#gid=0';
+// Name of the specific sheet in the spreadsheet.
+var SHEET_NAME = 'log';
+
+function updateAccountsInSeries() {
+  // You can use this approach when you have only minimal processing to
+  // perform in each of your client accounts.
+  
+  // clear current log
+  clearSheetData();
+  
+  // Select the accounts to be processed.
+  var accountIterator = MccApp.accounts()
+      .withCondition("LabelNames CONTAINS 'BE'")
+      .get();
+
+  // Save the MCC account, to switch back later.
+  var mccAccount = AdWordsApp.currentAccount();
+    
+  appendARow(['Campaign', 'Adgroup', 'New Adgroup']);
+  
+  // config
+  var findThis = /^./i;
+  var replaceWith = "";
+  
+    while (accountIterator.hasNext()) {
+      var account = accountIterator.next();
+
+      // Switch to the account you want to process.
+      MccApp.select(account);
+
+    // AdWordsApp.adGroups() will return all ad groups that are not removed by
+    // default.
+    var adGroupIterator = AdWordsApp.adGroups()
+    .withCondition("Status != REMOVED")
+    .withCondition("CampaignStatus != REMOVED")
+    .withCondition('CampaignName DOES_NOT_CONTAIN _DSR_')
+    .withCondition('CampaignName DOES_NOT_CONTAIN _DST_')
+    .withCondition('CampaignName DOES_NOT_CONTAIN _YT_')
+    .withCondition('CampaignName DOES_NOT_CONTAIN _DSA_')
+    //.withCondition('Name CONTAINS "Branding"')
+    .get();
+
+      while (adGroupIterator.hasNext()) {
+        var adGroup = adGroupIterator.next();
+        
+        // get the campaignname for this adgroup just for info
+        var campaignName = adGroup.getCampaign().getName();
+        
+        // define how the new name should be formatted
+        var newName = adGroup.getName().replace(findThis, replaceWith);
+        
+        // new adgroup structure for merge
+        //var matchTypeRegEx = /[A-Z]*$/;
+        //var matchType = matchTypeRegEx.exec(campaignName);
+        
+      //var groupRemove = /(CP|SP|VN)_(BEFR|BENL)_(B|NB|DSR|DST|YT|UAC)(_(PARK|DSA|DYNAMIC_X|GMAIL|TEST))?(_(COUNTRY|LASTMINUTE|PROMO|REGION|ACTIVITY|SEASONAL|CITY|TARGET|EVENTS|ATTRACTION|DURATION|HOLIDAY|DEPARTMENT|ACCOMMODATION|BRANDING))?/i;
+        //var groupRemove2 = /(_MBM|_EXACT)$/i;
+        
+        //var group = campaignName.replace(groupRemove,'').replace(groupRemove2,'');
+                
+        //var newName = matchType[0].toUpperCase() + group.toUpperCase() + ' # ' + adGroup.getName();
+        
+        Logger.log(newName);
+        
+    
+        if ( adGroup.getName() != newName ) {
+          // do a log before making the change
+          appendARow([campaignName, adGroup.getName(), newName]);
+          // set new campaign names by replacing current name using a regex.
+          adGroup.setName(newName);
+
+          
+        }
+      }
+    }
+  
+}
+
+function clearSheetData() {
+
+
+  var ss = SpreadsheetApp.openByUrl(SPREADSHEET_URL);
+  var sheet = ss.getSheetByName(SHEET_NAME);
+  sheet.clearContents();
+}
+
+function appendARow(rowArray) {
+
+  var ss = SpreadsheetApp.openByUrl(SPREADSHEET_URL);
+  var sheet = ss.getSheetByName(SHEET_NAME);
+
+  // Appends a new row with 3 columns to the bottom of the
+  // spreadsheet containing the values in the array.
+  sheet.appendRow(rowArray);
+}
+
+function main() {
+  updateAccountsInSeries();
+}
+
--- BE_Mass_Campaign_Rename.js
+++ BE_Mass_Campaign_Rename.js
@@ -1,15 +1,26 @@
+// log to this spreadsheet
+var SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/13yEIQYdiTlVVKySrgtidp_UNosy41zjZ6iZxP1DG8XA/edit#gid=0';
+// Name of the specific sheet in the spreadsheet.
+var SHEET_NAME = 'log';
+
 function updateAccountsInSeries() {
   // You can use this approach when you have only minimal processing to
   // perform in each of your client accounts.
+  
+  // clear current log
+  clearSheetData();
 
   // Select the accounts to be processed.
   var accountIterator = MccApp.accounts()
-      //.withCondition("LabelNames CONTAINS 'BE'")
-      .withIds([
-        //'665-'
-        //,'314-'
-      ])
+      .withCondition('LabelNames CONTAINS "VNBE"')
+  	  .withCondition('LabelNames CONTAINS "BEFR"')
       .get();
+  
+  appendARow(['Campaign', 'New Campaign', 'Conversions', 'Clicks']);
+  
+  // config
+  var findThis = /_HOLIDAY_GENERAL_/i;
+  var replaceWith = "_HOLIDAY_";
 
   // Save the MCC account, to switch back later.
   var mccAccount = AdWordsApp.currentAccount();
@@ -20,24 +31,57 @@ function updateAccountsInSeries() {
     // Switch to the account you want to process.
     MccApp.select(account);
 
-    // Retrieve all campaigns to be paused.
+    // Retrieve all campaigns to be processed.
     var campaignIterator = AdWordsApp.campaigns()
-        //.withCondition("Name CONTAINS 'Season'")
+    	.withCondition('Status != REMOVED')
+        .withCondition('Name DOES_NOT_CONTAIN "_DSR_"')
+    	.withCondition('Name DOES_NOT_CONTAIN "_DST_"')
+    	.withCondition('Name DOES_NOT_CONTAIN "_DSA_"')
+    	.withCondition('Name DOES_NOT_CONTAIN "_YT_"')
+     	.forDateRange("LAST_30_DAYS")
+    	.orderBy("Conversions DESC") 	
+    	.orderBy("Clicks DESC")
         .get();
 
     while (campaignIterator.hasNext()) {
       var campaign = campaignIterator.next();
+      var campaignName = campaign.getName();
       
-      // post info to logs
-      Logger.log('campaign %s to %s\nin account %s - %s'
-                 , campaign.getName(), campaign.getName().replace(/_ /i, "_"),
-                 account.getCustomerId(), account.getName()
-                );
+      // define new campaign name
+      //var campaignNewName = campaign.getName().replace(findThis, replaceWith);
       
+      // preparation for the merge
+      var groupRegEx = /((CP|SP|VN)_(BEFR|BENL)_(B|NB|DSR|DST|YT|UAC)(_(PARK|DSA|DYNAMIC_X|GMAIL|TEST))?(_(COUNTRY|LASTMINUTE|PROMO|REGION|ACTIVITY|SEASONAL|CITY|TARGET|EVENTS|ATTRACTION|DURATION|HOLIDAY|DEPARTMENT|ACCOMMODATION|BRANDING))?)/i;
+      var campaignNewName = groupRegEx.exec(campaignName)[0].toUpperCase();
+      
+      
+      
+      // use preview to test!
+      Logger.log(campaignNewName);
+      appendARow([campaignName, campaignNewName, campaign.getStatsFor('LAST_30_DAYS').getConversions(), campaign.getStatsFor('LAST_30_DAYS').getClicks()]);
       // set new campaign names by replacing current name using a regex.
-      campaign.setName(campaign.getName().replace(/^BE(NL|FR)_/i, campaign.getName().substring(0,4) + "_VN_"));
+      campaign.setName(campaignNewName);
+      
     }
   }
+}
+
+function clearSheetData() {
+
+
+  var ss = SpreadsheetApp.openByUrl(SPREADSHEET_URL);
+  var sheet = ss.getSheetByName(SHEET_NAME);
+  sheet.clearContents();
+}
+
+function appendARow(rowArray) {
+
+  var ss = SpreadsheetApp.openByUrl(SPREADSHEET_URL);
+  var sheet = ss.getSheetByName(SHEET_NAME);
+
+  // Appends a new row with 3 columns to the bottom of the
+  // spreadsheet containing the values in the array.
+  sheet.appendRow(rowArray);
 }
 
 function main() {
