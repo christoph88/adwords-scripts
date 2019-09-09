af6a592b91609b01f56285174571007b2cbe8a21
Christoph Geypen <geypenchristoph@gmail.com>
2019-09-09 15:02:05 +0200

    completed cross adgroup negatives script

--- MV_cross_adgroup_negatives.js
+++ MV_cross_adgroup_negatives.js
@@ -1,62 +1,93 @@
+/*
+Get all ad groups
+Label all adgroups
+Convert the ad group names to keywords
+Create an object with adgroups and all negatives
+Filter short tail negatives
+Push negatives to adgroup
+Remove adgroup label
+Check if any labels are still present, rinse and repeat.
+
+*/
+
+// GLOBAL VARIABLES
+
+var adgroups = {};
+var labelName = 'script - cankw';
+var resumeRun = resumeRun();
+
+// MAIN
+
 function main() {
-  Logger.log('check https://docs.google.com/spreadsheets/d/1Db6uwMTHTEJrCeiuXoSnMW7v93KQ9KvgvryUBrQlUVk/edit#gid=0');
-  removeNegativeKeywordsFromAdGroups()
-  resetSheet()
-  // only add negatives to search ad groups
-  getKeywordsInAdGroup("_search", "_B_");
-  // get NB seperately because they can't be added to branded campaigns
-  getKeywordsInAdGroup("_search", "_NB_");
+  //removeNegativeKeywordsFromAdGroups();
+  Logger.log('check https://docs.google.com/spreadsheets/d/1Db6uwMTHTEJrCeiuXoSnMW7v93KQ9KvgvryUBrQlUVk/edit#gid=0'); 
+  
+  if (!resumeRun) {
+    resetSheet();
+    createLabel();
+    removeNegativeKeywordsFromAdGroups()
+  }
+  
+  // add function to remove the "to add keyword from all adgroups:.
+  // if label does not exist add specific label to all adgroups, if exists filter campaigns on that label for adding negatives
+  getAdGroupData();
 }
 
-function getKeywordsInAdGroup(campaigns, type) {
-  Logger.log('getting all keywords');
-  var keywordIterator = AdsApp.keywords()
-      .withCondition('CampaignName CONTAINS_IGNORE_CASE "' + campaigns + '"')
-      .withCondition('CampaignName CONTAINS_IGNORE_CASE "' + type + '"')
-      .withCondition('AdGroupStatus != REMOVED')    
-      .withCondition('CampaignStatus != REMOVED')
-      .get();
-  if (keywordIterator.hasNext()) {
-    while (keywordIterator.hasNext()) {
-      var keyword = keywordIterator.next();
-      addNegativeKeywordToAdGroup(campaigns, type, keyword.getText(), keyword.getAdGroup().getName(), keyword.getCampaign().getName());      
-    }
+
+
+// HELPERS
+
+function adgroupToNegativeMBM(adgroup) {
+  return '+'+ adgroup.replace(/\w+\#\w+ - /,'').replace(/( \- | )/g,' +');
+}
+
+function escapeRegExp(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+}
+
+function createLabel(){
+  AdsApp.createLabel(labelName, 'cross account negatives label', 'grey')
+}
+
+function resumeRun() {
+  try {
+   var totalLabelsAdded = AdsApp.adGroups()
+      .withCondition("LabelNames CONTAINS_ALL ['" + labelName + "']")
+      .get().totalNumEntities();
+ 
+  return totalLabelsAdded > 0;
+  } catch(err) { 
+    Logger.log(err);
+    return false; 
   }
 }
 
-function addNegativeKeywordToAdGroup(campaigns, type, keyword, currentAdGroup, currentCampaign) {
-  // add keywords to all other campaigns
-  // exclude current campaign
-  // exclude possible long tail campaigns based on this one
-  Logger.log('Add ' + keyword + ' to relevant adgroups');
-  
-  var shortTailExclusion = currentAdGroup || currentAdGroup.match(/\-.*$/g)[0];
-  var withinSameLanguage = currentCampaign.match(/_BE(NL|FR)_/g)[0];
-  
 
-    var adGroupIterator = AdsApp.adGroups()
-      // do not add to the current ad group
-      .withCondition('Name != "' + currentAdGroup + '"')
-      // do not add to ad groups having the same short tail
-      .withCondition('Name DOES_NOT_CONTAIN_IGNORE_CASE "' + shortTailExclusion + '"')
-      .withCondition('Status != REMOVED')    
-      .withCondition('CampaignStatus != REMOVED')
-      // only add to search campaigns
-      .withCondition('CampaignName CONTAINS_IGNORE_CASE "' + campaigns + '"') 
-      // stay within the same language
-      .withCondition('CampaignName CONTAINS_IGNORE_CASE "' + withinSameLanguage + '"') 
-      // do not add NB negatives to B campaigns
-      .withCondition('CampaignName CONTAINS_IGNORE_CASE "' + (type === '_NB_' ? '_NB_' : '_' ) + '"') 
-      .get();
+function appendARow(campaign, adgroup, keyword) {
+  var SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1Db6uwMTHTEJrCeiuXoSnMW7v93KQ9KvgvryUBrQlUVk/edit#gid=0';
+  // Name of the specific sheet in the spreadsheet.
+  var SHEET_NAME = 'data';
+
+  var ss = SpreadsheetApp.openByUrl(SPREADSHEET_URL);
+  var sheet = ss.getSheetByName(SHEET_NAME);
+ 
   
-  if ( adGroupIterator.hasNext() ) {
-    var adGroup = adGroupIterator.next();
-    appendARow(currentCampaign, currentAdGroup, keyword, adGroup.getCampaign().getName(), adGroup.getName());
-    adGroup.createNegativeKeyword(keyword);
-  
-  }
-  
-  
+  // append rows
+  sheet.appendRow([new Date(),campaign, adgroup, '="' + keyword + '"']);
+}
+
+function resetSheet() {
+  var SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1Db6uwMTHTEJrCeiuXoSnMW7v93KQ9KvgvryUBrQlUVk/edit#gid=0';
+  // Name of the specific sheet in the spreadsheet.
+  var SHEET_NAME = 'data';
+
+  var ss = SpreadsheetApp.openByUrl(SPREADSHEET_URL);
+  var sheet = ss.getSheetByName(SHEET_NAME);
+ 
+  sheet.clear();
+  // append rows
+  sheet.appendRow(['timestamp','campaign', 'adGroup', 'negative']);
+ 
 }
 
 function removeNegativeKeywordsFromAdGroups() {
@@ -80,31 +111,114 @@ function removeNegativeKeywordsFromAdGroups() {
   }
 }
 
-function appendARow(currentCampaign, currentAdgroup, keyword, toCampaign, toAdgroup) {
-  var SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1Db6uwMTHTEJrCeiuXoSnMW7v93KQ9KvgvryUBrQlUVk/edit#gid=0';
-  // Name of the specific sheet in the spreadsheet.
-  var SHEET_NAME = 'data';
 
-  var ss = SpreadsheetApp.openByUrl(SPREADSHEET_URL);
-  var sheet = ss.getSheetByName(SHEET_NAME);
- 
+
+// MAIN FUNCTIONS
+
+function getAdGroupData() {  
+
   
-  // append rows
-  //sheet.appendRow(['timestamp','currentCampaign', 'currentAdgroup', 'keyword', 'toCampaign', 'toAdgroup']);
-  sheet.appendRow([new Date(),currentCampaign, currentAdgroup, '="' + keyword + '"', toCampaign, toAdgroup]);
+  Logger.log('push adgroup data to object');
+    var adgroupIterator = AdsApp.adGroups()
+      .withCondition('AdGroupStatus != REMOVED')    
+      .withCondition('CampaignStatus != REMOVED')
+      .withCondition('CampaignName CONTAINS "_Search_"'  )
+      .get();
+  
+  Logger.log(adgroupIterator.totalNumEntities());
+  
+  if (adgroupIterator.hasNext()) {
+    while (adgroupIterator.hasNext()) {
+      var adgroup = adgroupIterator.next();
+      adgroups[adgroup.getId()] = {};
+      adgroups[adgroup.getId()]['name'] = adgroup.getName();
+      adgroups[adgroup.getId()]['keyword'] = adgroupToNegativeMBM(adgroup.getName());
+      adgroups[adgroup.getId()]['negatives'] = [];
+      
+      if (!resumeRun) {
+        adgroup.applyLabel(labelName);
+      }
+      
+    }
+  }
+  
+  //Logger.log(adgroups);
+  processAdGroups();
 }
 
-function resetSheet() {
-  var SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1Db6uwMTHTEJrCeiuXoSnMW7v93KQ9KvgvryUBrQlUVk/edit#gid=0';
-  // Name of the specific sheet in the spreadsheet.
-  var SHEET_NAME = 'data';
+function processAdGroups() {
+  Logger.log('Loop through ad groups');
+  Logger.log('Push negative keywords per adgroup to object');
 
-  var ss = SpreadsheetApp.openByUrl(SPREADSHEET_URL);
-  var sheet = ss.getSheetByName(SHEET_NAME);
- 
-  sheet.clear();
-  // append rows
-  sheet.appendRow(['timestamp','currentCampaign', 'currentAdgroup', 'keyword', 'toCampaign', 'toAdgroup']);
- 
+  for (var i = 0; i < Object.keys(adgroups).length; i++) {
+    var adgroupId = Object.keys(adgroups)[i];
+    var adgroupKeyword = adgroups[adgroupId].keyword;
+    
+    addToAdgroupNegatives(adgroupId, adgroupKeyword);
+
+  }
+  pushNegativesToAds();
 }
 
+
+function addToAdgroupNegatives(currentAdgroupId, currentAdgroupKeyword) {
+  
+  // add the negative keyword to all relevant adgroups
+  for (var i = 0; i < Object.keys(adgroups).length; i++) {
+    var adgroupId = Object.keys(adgroups)[i];
+    var adgroupNegatives = adgroups[adgroupId].negatives;
+    
+    if (currentAdgroupId !== adgroupId) {
+      adgroupNegatives.push(currentAdgroupKeyword);
+    }
+    
+  }
+}
+
+
+
+function pushNegativesToAds() {
+  
+  Logger.log('get negatives from object and push them to the adgroups');
+  
+  var adgroupIterator = AdsApp.adGroups()
+      .withCondition("LabelNames CONTAINS_ALL ['" + labelName + "']")
+      .get();
+  
+  Logger.log(adgroupIterator.totalNumEntities() + ' ad groups to process');
+  
+  if (adgroupIterator.hasNext()) {
+    while (adgroupIterator.hasNext()) {
+      var adgroup = adgroupIterator.next();      
+      var adgroupId = adgroup.getId();
+      
+      // add negatives to adgroup > getnegativesexcluding adgroup
+      if (adgroups[adgroupId]) {
+        
+        var name = adgroups[adgroupId].name;
+        var negatives = adgroups[adgroupId].negatives;
+
+        Logger.log('Processing - ' + name);
+        Logger.log(negatives.length);
+        
+        for (var i = 0; i < negatives.length; i++) {
+          var negative = negatives[i];
+          
+          // check if negative is short tail
+          var cleanNeg = negative.replace(/(\+|\*)/g,'');
+          var shortTail = name.match(new RegExp(cleanNeg));
+          
+          if (shortTail == null){
+            appendARow(adgroup.getCampaign().getName(), adgroup.getName(), negative);
+            adgroup.createNegativeKeyword(negative);          
+          }
+          
+
+        } 
+      }
+      Logger.log('remove label from ' + name);
+      adgroup.removeLabel(labelName);
+    }
+  }
+
+}
